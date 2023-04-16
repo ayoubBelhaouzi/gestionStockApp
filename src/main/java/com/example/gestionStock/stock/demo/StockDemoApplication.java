4974146a0f087ae2626b98dd19f08c44a6641e1b
ayoubBelhaouzi <ayoub.belhaouzi@gmail.com>
2023-04-16 04:22:50 +0200

    create-app-server

---
+++ src/main/java/com/example/gestionStock/stock/demo/StockDemoApplication.java
@@ -0,0 +1,18 @@
+package com.example.gestionStock.stock.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockDemoApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockDemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello");
+	}
+}
